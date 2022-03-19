aeed5f1cc2fe19cd0928d46732c4b208345de875
Joshua Hales <A02212327@usu.edu>
2022-03-18 20:10:10 -0600

    Stubbed in methods for Cube

--- src/Cube.java
+++ src/Cube.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Brief Description
@@ -47,5 +48,71 @@ public class Cube {
                     }
                 };
         this.history = new ArrayList<>();
+    }
+
+    public Cube (String[][][] tiles) {
+        this.tiles = new String[3][3][3];
+        for (int face = 0; face < tiles.length; face++) {
+            for (int row = 0; row < tiles[face].length; row++) {
+                this.tiles[face][row] = tiles[face][row].clone();
+            }
+        }
+        this.history = new ArrayList<>();
+    }
+
+    public void scramble() {
+
+    }
+
+    public boolean isValid() {
+        return false;
+    }
+
+    public boolean isSolved() {
+        return false;
+    }
+
+    public String getTile(int face, int tile) {
+        return tiles[face - 1][(tile%3) - 1][(tile%3) - 1];
+    }
+
+    public ArrayList<String> getHistory() {
+        return history;
+    }
+
+    @Override
+    public String toString() {
+        return "Cube{" +
+                "tiles=" + Arrays.toString(tiles) +
+                ", history=" + history +
+                '}';
+    }
+
+    public void rotate(String[] commands) {
+
+    }
+
+    private void rotateUp() {
+
+    }
+
+    private void rotateDown() {
+
+    }
+
+    private void rotateLeft() {
+
+    }
+
+    private void rotateRight() {
+
+    }
+
+    private void rotateFront() {
+
+    }
+
+    private void rotateBack() {
+
     }
 }
