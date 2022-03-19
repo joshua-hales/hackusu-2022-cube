a062fd49d814e86492cb07297b660f59929a44fc
aaronmhales <a02353892@aggies.usu.edu>
2022-03-18 21:31:59 -0600

    finished Card.rotateUp(), need to be tested:

--- src/Cube.java
+++ src/Cube.java
@@ -12,8 +12,50 @@ public class Cube {
 
     private String[][][] tiles;
     private ArrayList<String> history;
+    private final String[][][] solved = new String[][][]
+            {
+                    {
+                            {"W","W","W"},
+                            {"W","W","W"},
+                            {"W","W","W"}
+                    },
+                    {
+                            {"R","R","R"},
+                            {"R","R","R"},
+                            {"R","R","R"}
+                    },
+                    {
+                            {"B","B","B"},
+                            {"B","B","B"},
+                            {"B","B","B"}
+                    },
+                    {
+                            {"O","O","O"},
+                            {"O","O","O"},
+                            {"O","O","O"}
+                    },
+                    {
+                            {"G","G","G"},
+                            {"G","G","G"},
+                            {"G","G","G"}
+                    },
+                    {
+                            {"Y","Y","Y"},
+                            {"Y","Y","Y"},
+                            {"Y","Y","Y"}
+                    }
+            };
 
     public Cube() {
+
+        /*
+         * W = White
+         * R = Red
+         * B = Blue
+         * O = Orange
+         * G = Green
+         * Y = Yellow
+         */
         this.tiles = new String[][][]
                 {
                     {
@@ -69,10 +111,26 @@ public class Cube {
     }
 
     public boolean isSolved() {
-        return false;
+        boolean solved = true;
+        while (solved) {
+            for (int side = 0; side < this.tiles.length; side++) {
+                for (int face = 0; face < tiles.length; face++) {
+                    for (int row = 0; row < this.tiles[face].length; row++) {
+                        if (!this.tiles[side][face][row].equals(this.solved[side][face][row])) {
+                            solved = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return solved;
     }
 
     public String getTile(int face, int tile) {
+        if (tile % 3 == 0) {
+            tile = 4;
+        }
         return tiles[face - 1][(tile%3) - 1][(tile%3) - 1];
     }
 
@@ -93,7 +151,63 @@ public class Cube {
     }
 
     private void rotateUp() {
+        String[][] currentTop = new String[3][3];
+        for (int row = 0; row < this.tiles[0].length; row++) {
+            for (int tile = 0; tile < this.tiles[0][0].length; tile++) {
+                currentTop[row][tile] = this.tiles[0][row][tile];
+            }
+        }
+        String[][] currentSides = new String[3][3];
+        for (int face = 0; face < 5; face++) {
+            for (int tile = 0; tile < this.tiles[0][0].length; tile++) {
+                currentSides[face][tile] = this.tiles[face + 1][0][tile];
+            }
+        }
+        String[] currentSideRow = new String[3];
+        String[] tempSideRow = new String[3];
+        for (int face = 0; face < 4; face++) {
+            for (int tile = 0; tile < this.tiles[face][face].length; tile++) {
+                currentSideRow[tile] = currentSides[face][tile];
+                tempSideRow[tile] = currentSides[3 - face][tile];
+                currentSides[3 - face][tile] = currentSideRow[tile];
+                currentSideRow[tile] = tempSideRow[tile];
+            }
+        }
+        String tempTile = "";
+        String tempTileToReplace = "";
 
+        tempTile = this.tiles[0][0][2];
+        this.tiles[0][0][2] = this.tiles[0][0][0];
+        tempTileToReplace = this.tiles[0][2][2];
+        this.tiles[0][2][2] = tempTile;
+        tempTile = tempTileToReplace;
+
+        tempTileToReplace = this.tiles[0][2][0];
+        this.tiles[0][2][0] = tempTile;
+        tempTile = tempTileToReplace;
+
+        tempTileToReplace = this.tiles[0][0][0];
+        this.tiles[0][0][0] = tempTile;
+
+
+        tempTile = this.tiles[0][1][2];
+        this.tiles[0][1][2] = this.tiles[0][0][1];
+        tempTileToReplace = this.tiles[0][2][1];
+        this.tiles[0][1][2] = tempTile;
+        tempTile = tempTileToReplace;
+
+        tempTileToReplace = this.tiles[0][1][0];
+        this.tiles[0][1][0] = tempTile;
+        tempTile = tempTileToReplace;
+
+        tempTileToReplace = this.tiles[0][1][0];
+        this.tiles[0][1][0] = tempTile;
+
+        for (int face = 1; face < 4; face++) {
+            for (int tile = 0; tile < this.tiles[0][0].length; tile++) {
+                this.tiles[face][0][tile] = currentSides[face - 1][tile];
+            }
+        }
     }
 
     private void rotateDown() {
