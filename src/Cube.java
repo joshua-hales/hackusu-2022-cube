675ec83e736ca75f1c56fd1a51aea23daec17fc2
aaronmhales <a02353892@aggies.usu.edu>
2022-03-18 19:30:24 -0600

    default constructor of cude class

--- src/Cube.java
+++ src/Cube.java
@@ -6,4 +6,42 @@
  * @author Joshua Hales and Aaron Hales
  */
 public class Cube {
+
+    private String[][][] tiles;
+
+    public Cube() {
+        this.tiles = new String[][][]
+                {
+                    {
+                        {"W","W","W"},
+                        {"W","W","W"},
+                        {"W","W","W"}
+                    },
+                    {
+                        {"R","R","R"},
+                        {"R","R","R"},
+                        {"R","R","R"}
+                    },
+                    {
+                        {"B","B","B"},
+                        {"B","B","B"},
+                        {"B","B","B"}
+                    },
+                    {
+                        {"O","O","O"},
+                        {"O","O","O"},
+                        {"O","O","O"}
+                    },
+                    {
+                        {"G","G","G"},
+                        {"G","G","G"},
+                        {"G","G","G"}
+                    },
+                    {
+                        {"Y","Y","Y"},
+                        {"Y","Y","Y"},
+                        {"Y","Y","Y"}
+                    }
+                };
+    }
 }
