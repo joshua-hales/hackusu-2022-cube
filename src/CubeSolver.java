7cff7980bd400dfb2f8cf518810e8a830afba42d
Joshua Hales <A02212327@usu.edu>
2022-03-18 19:25:37 -0600

    Stubbed in methods for CubeSolver

--- src/CubeSolver.java
+++ src/CubeSolver.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Brief Description
  * <p>
@@ -6,4 +8,51 @@
  * @author Joshua Hales and Aaron Hales
  */
 public class CubeSolver {
+    private Cube cube;
+    private ArrayList<String> history;
+
+    public CubeSolver(Cube cube) {
+        this.cube = cube;
+        this.history = new ArrayList<>();
+    }
+
+    public void solve() {
+
+    }
+
+    public ArrayList<String> getHistory() {
+        return history;
+    }
+
+    private void solveTop() {
+
+    }
+
+    private void whiteCross() {
+
+    }
+
+    private void whiteCorners() {
+
+    }
+
+    private void solveMid() {
+
+    }
+
+    private void solveBottom() {
+
+    }
+
+    private void yellowCross() {
+
+    }
+
+    private void yellowCorners() {
+
+    }
+
+    private void yellowEdges() {
+
+    }
 }
