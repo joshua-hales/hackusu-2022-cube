5ae12311b057d7b2a2de1ee86aca9b1623e563f0
aaronmhales <a02353892@aggies.usu.edu>
2022-03-19 02:09:53 -0600

    finished Card.rotateRight()

--- src/Cube.java
+++ src/Cube.java
@@ -154,7 +154,7 @@ public class Cube {
     }
 
     public void rotate(String[] commands) {
-        rotateLeft();
+        rotateRight();
     }
 
     private void rotateUp() {
@@ -274,7 +274,7 @@ public class Cube {
 
     private void rotateLeft() {
         String[][] currentLeft = new String[3][3];
-        for (int row = 0; row < this.tiles[5].length; row++) {
+        for (int row = 0; row < this.tiles[4].length; row++) {
             currentLeft[row] = this.tiles[4][row].clone();
         }
 
@@ -299,7 +299,7 @@ public class Cube {
         currentLeft[1][0] = tempTile2;
         currentLeft[0][1] = tempTile;
 
-        for (int row = 0; row < this.tiles[5].length; row++) {
+        for (int row = 0; row < this.tiles[4].length; row++) {
             this.tiles[4][row] = currentLeft[row].clone();
         }
 
@@ -343,7 +343,73 @@ public class Cube {
     }
 
     private void rotateRight() {
+        String[][] currentLeft = new String[3][3];
+        for (int row = 0; row < this.tiles[2].length; row++) {
+            currentLeft[row] = this.tiles[2][row].clone();
+        }
 
+        String tempTile = "";
+        String tempTile2 = "";
+
+        tempTile = currentLeft[2][0];
+        tempTile2 = currentLeft[0][0];
+        currentLeft[2][0] = currentLeft[2][2];
+        currentLeft[0][0] = tempTile;
+        tempTile = tempTile2;
+        tempTile2 = currentLeft[0][2];
+        currentLeft[0][2] = tempTile;
+        tempTile = tempTile2;
+        currentLeft[2][2] = tempTile;
+
+        tempTile = currentLeft[1][2];
+        currentLeft[1][2] = currentLeft[0][1];
+        tempTile2 = currentLeft[2][1];
+        currentLeft[2][1] = tempTile;
+        tempTile = currentLeft[1][0];
+        currentLeft[1][0] = tempTile2;
+        currentLeft[0][1] = tempTile;
+
+        for (int row = 0; row < this.tiles[2].length; row++) {
+            this.tiles[2][row] = currentLeft[row].clone();
+        }
+
+        String[][] currentSides = new String[4][3];
+        currentSides[0][0] = this.tiles[0][2][2];
+        currentSides[0][1] = this.tiles[0][1][2];
+        currentSides[0][2] = this.tiles[0][0][2];
+        currentSides[1][0] = this.tiles[3][0][0];
+        currentSides[1][1] = this.tiles[3][1][0];
+        currentSides[1][2] = this.tiles[3][2][0];
+        currentSides[2][0] = this.tiles[5][0][0];
+        currentSides[2][1] = this.tiles[5][1][0];
+        currentSides[2][2] = this.tiles[5][2][0];
+        currentSides[3][0] = this.tiles[1][2][2];
+        currentSides[3][1] = this.tiles[1][1][2];
+        currentSides[3][2] = this.tiles[1][0][2];
+
+        String[] tempRow = new String[3];
+        String[] tempRow2 = new String[3];
+        tempRow = currentSides[1].clone();
+        tempRow2 = currentSides[2].clone();
+        currentSides[1] = currentSides[0].clone();
+        currentSides[2] = tempRow.clone();
+        tempRow = tempRow2.clone();
+        tempRow2 = currentSides[3].clone();
+        currentSides[3] = tempRow.clone();
+        currentSides[0] = tempRow2.clone();
+
+        this.tiles[0][2][2] = currentSides[0][0];
+        this.tiles[0][1][2] = currentSides[0][1];
+        this.tiles[0][0][2] = currentSides[0][2];
+        this.tiles[3][0][0] = currentSides[1][0];
+        this.tiles[3][1][0] = currentSides[1][1];
+        this.tiles[3][2][0] = currentSides[1][2];
+        this.tiles[5][0][0] = currentSides[2][0];
+        this.tiles[5][1][0] = currentSides[2][1];
+        this.tiles[5][2][0] = currentSides[2][2];
+        this.tiles[1][2][2] = currentSides[3][0];
+        this.tiles[1][1][2] = currentSides[3][1];
+        this.tiles[1][0][2] = currentSides[3][2];
     }
 
     private void rotateFront() {
